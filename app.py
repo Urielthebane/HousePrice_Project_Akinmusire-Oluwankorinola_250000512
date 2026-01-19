b4f69684b1ba6c6eaab39fded38b97aa85099008
Urielthebane <oluwankorinolaa@gmail.com>
2026-01-19 03:59:44 +0100

    new changes

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import joblib, json
+import pandas as pd
+
+app = Flask(__name__)
+
+model = joblib.load("model/house_price_model.pkl")
+scaler = joblib.load("model/feature_columns.pkl")
+columns = json.load(open("model/columns.json"))
+
+num_cols = [
+    "OverallQual","GrLivArea","TotalBsmtSF",
+    "GarageCars","BedroomAbvGr","FullBath","YearBuilt"
+]
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        data = {
+            "OverallQual": float(request.form["OverallQual"]),
+            "GrLivArea": float(request.form["GrLivArea"]),
+            "TotalBsmtSF": float(request.form["TotalBsmtSF"]),
+            "GarageCars": float(request.form["GarageCars"]),
+            "BedroomAbvGr": float(request.form["BedroomAbvGr"]),
+            "FullBath": float(request.form["FullBath"]),
+            "YearBuilt": float(request.form["YearBuilt"]),
+            "Neighborhood": request.form["Neighborhood"]
+        }
+
+        df = pd.DataFrame([data])
+        df = pd.get_dummies(df, columns=["Neighborhood"], drop_first=True)
+
+        for col in columns:
+            if col not in df.columns:
+                df[col] = 0
+        df = df[columns]
+
+        df[num_cols] = scaler.transform(df[num_cols])
+        prediction = model.predict(df)[0]
+
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    print("Starting Flask server...")
+    print("Open this link in your browser: http://127.0.0.1:5000")
+    app.run(host="0.0.0.0", port=5000, debug=True)
